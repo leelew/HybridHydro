f7df42fd75cf4c7d022b956182d694bf52d93a4d
leelew <lilu83@mail.sysu.edu.cn>
2022-10-01 22:07:26 +0800

    Remove plot codes

--- postprocess.sh
+++ postprocess.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-saved_model_path="/tera08/lilu/HybridHydro/output/saved_model/"
-saved_forecast_path="/tera08/lilu/HybridHydro/output/forecast/"
-outputs_path="/tera08/lilu/HybridHydro/output/"
-inputs_path="/tera08/lilu/HybridHydro/input/"
-model_name="convlstm"
+saved_model_path="/tera05/lilu/HybridHydro/output/saved_model/"
+saved_forecast_path="/tera05/lilu/HybridHydro/output/forecast/"
+outputs_path="/tera05/lilu/HybridHydro/output/"
+inputs_path="/tera05/lilu/HybridHydro/input/"
+model_name="convlstm_condition"
 
-python3 -B src/postprocess.py --id $i --epochs $epochs --saved_model_path $saved_model_path --saved_forecast_path $saved_forecast_path --inputs_path $inputs_path --outputs_path $outputs_path --batch_size $batch_size --model_name $model_name
+#python3 -B src/postprocess.py --saved_model_path $saved_model_path --saved_forecast_path $saved_forecast_path --inputs_path $inputs_path --outputs_path $outputs_path --model_name $model_name
 
-python3 -B src/cal_perf.py --id $i --epochs $epochs --saved_model_path $saved_model_path --saved_forecast_path $saved_forecast_path --inputs_path $inputs_path --outputs_path $outputs_path --batch_size $batch_size --model_name $model_name
+python3 -B src/cal_perf.py --saved_model_path $saved_model_path --saved_forecast_path $saved_forecast_path --inputs_path $inputs_path --outputs_path $outputs_path --model_name $model_name
